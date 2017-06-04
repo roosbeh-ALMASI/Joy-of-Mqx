1a37b1515c366f164e3bdd5cc5253e3a02d9b0bf
Roosbeh Almasi <roosbeh.almasi@Millfield.local>
2017-06-04 13:16:23 +0100

    LPTMR and WatchDog working ok, timer kickes wdog every 16ms and wdog reset sys in 1.5s if not kicked, generates isq too before reset sys

--- MainLoop.c
+++ MainLoop.c
@@ -8,16 +8,19 @@ typedef enum{
 
 }_BOOL;
 
+
+
 _BOOL  isLptmrRunning   =  _FALSE;
 _BOOL  isWatchdogKicked =  _FALSE;
 
 
 
 
-void LptmrInit(void);
+
+void LptmrInit(uint16_t ms);
 void wdog_init(uint32_t ms);
 void wdog_disable(void);
-void wdog_kick(void);
+
 
 
 #define PRREG(z) PRINTF(#z" 0x%x\r\n", z);
@@ -29,8 +32,8 @@ void MainLoop(void)
 {
 
 
-    LptmrInit();
-    wdog_init(20);
+    LptmrInit(16);
+    wdog_init(1500);
 
     PRINTF("system just entering while loop\r\n");
 
@@ -48,60 +51,13 @@ void MainLoop(void)
         }
 
 
+
+
    }
-
-
-
-/*
-
-	int i,fbus,flashbus;
-
-	    fbus = SystemCoreClock / (((SIM->CLKDIV1 & SIM_CLKDIV1_OUTDIV2_MASK) >> SIM_CLKDIV1_OUTDIV2_SHIFT) + 1);
-	    flashbus = SystemCoreClock / (((SIM->CLKDIV1 & SIM_CLKDIV1_OUTDIV4_MASK) >> SIM_CLKDIV1_OUTDIV4_SHIFT) + 1);
-
-	    printf("\nSystemCoreClock %d fbus %d flash %d %s %s\n",  SystemCoreClock,  fbus,  flashbus,  __TIME__,  __DATE__);
-
-
-	    PRREG(RCM_SRS0);            // reset reason
-	    PRREG(RCM_SRS1);
-
-	    PRREG(WDOG->STCTRLH);
-	    PRREG(WDOG->PRESC);
-	    PRREG(WDOG->TOVALL);
-	    PRREG(WDOG->TOVALH);
-	    PRREG(WDOG->RSTCNT);
-
-	    wdog_init(4000);            // 4 sec timeout
-
-	    PRREG(WDOG->STCTRLH);
-	    PRREG(WDOG->PRESC);
-	    PRREG(WDOG->TOVALL);
-	    PRREG(WDOG->TOVALH);
-
-	    _time_delay(100);
-
-	    wdog_kick();
-
-	    for (i=1;i<=5; i++)
-	        {
-	            printf("running %d\n",i);
-	            _time_delay(1);
-	             wdog_kick();
-	        }
-
-	    printf("long wait reset\n");
-	    _time_delay(1);
-	    printf("never happen\n");
-
-
-	    */
 
 	
 }
 
-
-
-//-------------------------------------------------------------------------------------
 
 
 
@@ -117,6 +73,8 @@ void wdog_init(uint32_t ms)
     WDOG->TOVALL = (uint16_t)(ms & 0xffff);
     WDOG->TOVALH = (uint16_t)((ms>>16) & 0xffff);
 
+    // clk is LPO
+
     WDOG->STCTRLH = (WDOG_STCTRLH_ALLOWUPDATE_MASK   |   // uapdate regs En
     		              WDOG_STCTRLH_WDOGEN_MASK   |   // En
 						  WDOG_STCTRLH_WAITEN_MASK   |   // En in wait
@@ -127,6 +85,8 @@ void wdog_init(uint32_t ms)
     NVIC_EnableIRQ(WDOG_EWM_IRQn);
 
     __enable_irq();
+
+
 }
 
 void wdog_disable()
@@ -143,23 +103,16 @@ void wdog_disable()
     __enable_irq();
 }
 
-void wdog_kick()
+
+
+void LptmrInit(uint16_t ms)
 {
-     __disable_irq();
-    WDOG_REFRESH = 0xA602;
-    WDOG_REFRESH = 0xB480;
-    __enable_irq();
-}
-
-
-
-void LptmrInit(void)
-{
+	ms = ms*33;
 
 	SIM->SCGC5 |= 1UL;
 
 	LPTMR0->CSR = 0;
-	LPTMR0->CMR = 530;
+	LPTMR0->CMR = ms;
 	LPTMR0->PSR = 1UL<<2 | 1UL<<1;
 	LPTMR0->CSR = 1UL | 1UL<<6;
 
@@ -175,11 +128,15 @@ void LPTMR0_IRQHandler(void)
 	// A valid refresh is a write of 0xA602 followed by 0xB480
 	// within 20 bus clock cycles to watchdog refresh register
 
+	__disable_irq();
+
 	WDOG->REFRESH = 0xA602;
 	WDOG->REFRESH = 0xB480;
 
 	LPTMR0->CSR |= 1UL<<7;
 	isWatchdogKicked = _TRUE;
+
+	__enable_irq();
 
 }
 
@@ -187,12 +144,16 @@ void LPTMR0_IRQHandler(void)
 void WDOG_EWM_IRQHandler(void)
 {
 
-	//WDOG->UNLOCK = 0xC520;
-    //WDOG->UNLOCK = 0xD928;
+	__disable_irq();
 
-    //WDOG->STCTRLL |= 1UL<<15;  // CLEAR Interrupt flag
+	WDOG->UNLOCK = 0xC520;
+    WDOG->UNLOCK = 0xD928;
+    __asm("NOP");
+    __asm("NOP");
 
+    WDOG->STCTRLL |= WDOG_STCTRLL_INTFLG_MASK;  // CLEAR Interrupt flag
+
+    __enable_irq();
 }
-
 
 
