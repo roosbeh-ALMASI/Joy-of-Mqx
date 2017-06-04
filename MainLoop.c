954c399cd7f44a32e304e14a1970a3cc0b5a3e97
Roosbeh Almasi <roosbeh.almasi@Millfield.local>
2017-06-04 13:33:22 +0100

    some cleaning up on lptmr and watchdog system

--- MainLoop.c
+++ MainLoop.c
@@ -14,12 +14,9 @@ _BOOL  isLptmrRunning   =  _FALSE;
 _BOOL  isWatchdogKicked =  _FALSE;
 
 
-
-
-
-void LptmrInit(uint16_t ms);
-void wdog_init(uint32_t ms);
-void wdog_disable(void);
+void SysWdPet(uint16_t ms);
+void SysWdAdd(uint32_t ms);
+void SysWdDel(void);
 
 
 
@@ -32,8 +29,8 @@ void MainLoop(void)
 {
 
 
-    LptmrInit(16);
-    wdog_init(1500);
+	SysWdPet(16);
+	SysWdAdd(1500);
 
     PRINTF("system just entering while loop\r\n");
 
@@ -51,61 +48,15 @@ void MainLoop(void)
         }
 
 
-
-
    }
 
 	
 }
 
 
+//------------------------------------------------------------------------
 
-
-void wdog_init(uint32_t ms)
-{
-    __disable_irq();
-
-    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xC520);
-    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xD928);
-    __asm("NOP");
-    __asm("NOP");
-
-    WDOG->TOVALL = (uint16_t)(ms & 0xffff);
-    WDOG->TOVALH = (uint16_t)((ms>>16) & 0xffff);
-
-    // clk is LPO
-
-    WDOG->STCTRLH = (WDOG_STCTRLH_ALLOWUPDATE_MASK   |   // uapdate regs En
-    		              WDOG_STCTRLH_WDOGEN_MASK   |   // En
-						  WDOG_STCTRLH_WAITEN_MASK   |   // En in wait
-						  WDOG_STCTRLH_IRQRSTEN_MASK |   // IRQ En
-						  WDOG_STCTRLH_STOPEN_MASK);     // En in Stop
-    WDOG->PRESC = 0;
-
-    NVIC_EnableIRQ(WDOG_EWM_IRQn);
-
-    __enable_irq();
-
-
-}
-
-void wdog_disable()
-{
-    __disable_irq();
-
-    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xC520);
-    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xD928);
-    __asm("NOP");
-    __asm("NOP");
-
-    WDOG->STCTRLH = WDOG_STCTRLH_ALLOWUPDATE_MASK;
-
-    __enable_irq();
-}
-
-
-
-void LptmrInit(uint16_t ms)
+void SysWdPet(uint16_t ms)
 {
 	ms = ms*33;
 
@@ -121,12 +72,63 @@ void LptmrInit(uint16_t ms)
 }
 
 
+void SysWdAdd(uint32_t ms)
+{
+
+// watch dog module is using LPO clk= 1KHz, this could be changed to system bus clk
+    __disable_irq();
+
+    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xC520);
+    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xD928);
+    __asm("NOP");
+    __asm("NOP");
+
+    WDOG->TOVALL = (uint16_t)(ms & 0xffff);
+    WDOG->TOVALH = (uint16_t)((ms>>16) & 0xffff);
+
+
+    WDOG->STCTRLH = (WDOG_STCTRLH_ALLOWUPDATE_MASK   |   // uapdate regs En
+    		              WDOG_STCTRLH_WDOGEN_MASK   |   // En
+						  WDOG_STCTRLH_WAITEN_MASK   |   // En in wait
+						  WDOG_STCTRLH_IRQRSTEN_MASK |   // IRQ En
+						  WDOG_STCTRLH_STOPEN_MASK);     // En in Stop
+    WDOG->PRESC = 0;
+
+    NVIC_EnableIRQ(WDOG_EWM_IRQn);
+
+    __enable_irq();
+
+
+}
+
+void SysWdDel()
+{
+// We Could log our Reset Control Module (RCM) register here to
+// investigate the reason of the reset, or could do this right
+// after up coming boot process
+
+    __disable_irq();
+
+    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xC520);
+    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xD928);
+    __asm("NOP");
+    __asm("NOP");
+
+    WDOG->STCTRLH = WDOG_STCTRLH_ALLOWUPDATE_MASK;
+
+    __enable_irq();
+}
+
+
+
+
+//---------------------------------------------------------------------
 
 void LPTMR0_IRQHandler(void)
 {
-
-	// A valid refresh is a write of 0xA602 followed by 0xB480
-	// within 20 bus clock cycles to watchdog refresh register
+// LPTMR is using 32.768 KHz external clk, this could be changed
+// A valid refresh is a write of 0xA602 followed by 0xB480
+// within 20 bus clock cycles to watchdog refresh register
 
 	__disable_irq();
 
